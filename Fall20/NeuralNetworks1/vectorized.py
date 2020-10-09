c40e551d4740f6e4e51f0b9b555e1909d0edaef4
edwardyang12 <edwardyang016@gmail.com>
2020-10-08 23:10:40 -0700

    split

--- Fall20/NeuralNetworks1/vectorized.py
+++ Fall20/NeuralNetworks1/vectorized.py
@@ -3,6 +3,15 @@ import numpy as np
 def activation(z):
     return z
 
+def parametric_activation(a,z):
+    if z<=0:
+        return a*z
+    else:
+        return z
+
+def tanh_activation(z):
+    return np.tanh(z)
+
 # Simple 2 layer neural network that returns the average of a 3 numbers given as a 3x1 column vector
 # this function does a "forward pass" of the input x through the 2 layer network and returns the results
 def average_nn(x):
@@ -47,7 +56,27 @@ def random_nn(x):
 
     z_3 = np.matmul(w_3, a_2) + b_3
     a_3 = random_nn_activation(z_3)
-    
+
     return a_3
 
 print("On 3 layer network, input {} fed forward gives {}".format(x, random_nn(x)))
+
+# 4 layer NN for computing whether absolute difference is between 1 and 3
+# if between 1 and 3 outputs >0 else output <=0
+def multilayer(x):
+    w1 = np.array([1,-1])
+    b1 = 0
+    weighted_input1 = np.matmul(w1,x) + b1
+    output1 = parametric_activation(-1,weighted_input1)
+    w2 = np.array([1])
+    b2 = -2
+    weighted_input2 = np.matmul(w2,[output1]) + b2
+    output2 = parametric_activation(-1,weighted_input2)
+    w3 = np.array([-1])
+    b3 = 1
+    weighted_input3 = np.matmul(w3,[output2]) + b3
+    y = tanh_activation(weighted_input3)
+    return y
+
+x = np.array([4,5.5])
+print(multilayer(x))
